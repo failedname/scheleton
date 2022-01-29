82380bdc1f955213692b241a2c1e08d832aab4b0
javier nuñez <javier@MacBook-Pro-de-javier.local>
2022-01-28 22:26:27 -0500

    feat: add parser json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const config = require("./utils/config");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
